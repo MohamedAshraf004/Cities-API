6e94070c4e81f9f3e8d89b58b359b2ba38bd8d97
MohamedAshraf004 <lionelmido004@gmail.com>
2020-03-27 01:10:16 +0200

    Add Validation And CRUD For PointOfInterest

--- CityInfo.API/Controllers/PointsOfInterestController.cs
+++ CityInfo.API/Controllers/PointsOfInterestController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using CityInfo.API.Data;
+using CityInfo.API.Models;
+using CityInfo.API.ViewModels;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CityInfo.API.Controllers
@@ -24,7 +27,7 @@ namespace CityInfo.API.Controllers
         }
 
         // GET api/values/5
-        [HttpGet("{id}")]
+        [HttpGet("{id}",Name = "GetPointOFInterest")]
         public IActionResult GetPointOfInterestById(int cityId,int id)
         {
             var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
@@ -40,22 +43,126 @@ namespace CityInfo.API.Controllers
             return Ok(pointOfInterest);
         }
 
-        // POST api/values
+        
         [HttpPost]
-        public void Post([FromBody] string value)
+        public IActionResult CreatePointOfInterest(int cityId,[FromBody] PointOfInterestViewModel model)
         {
+            if (model.Description==model.Name)
+            {
+                ModelState.AddModelError("Description", "Description must be different from the name");
+            }
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+            if (city == null)
+            {
+                return NotFound();
+            }
+            var maxId = CitiesDataStore.Current.Cities.SelectMany(p => p.PointsOfInterest).Max(d => d.Id);
+            PointOfInterestDto pointOfInterest = new PointOfInterestDto
+            {
+                Id = ++maxId,
+                Name = model.Name,
+                Description = model.Description
+            };
+            city.PointsOfInterest.Add(pointOfInterest);
+            return CreatedAtRoute("GetPointOFInterest",new { cityId,id=pointOfInterest.Id},pointOfInterest);
+
         }
 
-        // PUT api/values/5
+        // PUT api/cities/cityId/point/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public IActionResult UpdatePointOfInterest(int cityId,int id, [FromBody] PointOfInterestViewModel model)
         {
+            if (model.Description == model.Name)
+            {
+                ModelState.AddModelError("Description", "Description must be different from the name");
+            }
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+            if (city == null)
+            {
+                return NotFound();
+            }
+            var pointOfInterest = city.PointsOfInterest.FirstOrDefault(p => p.Id == id);
+            if (pointOfInterest == null)
+            {
+                return NotFound();
+            }
+            pointOfInterest.Description = model.Description;
+            pointOfInterest.Name = model.Name;
+
+            return NoContent();
+        }
+        [HttpPatch("{id}")]
+        public IActionResult PartialUpdatePointOfInterest(int cityId, int id,
+                            [FromBody] JsonPatchDocument<PointOfInterestViewModel> patchDoc)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+            if (city == null)
+            {
+                return NotFound();
+            }
+            var pointOfInterestFromStore = city.PointsOfInterest.FirstOrDefault(p => p.Id == id);
+            if (pointOfInterestFromStore == null)
+            {
+                return NotFound();
+            }
+            var pointOfInterestTOPatch = new PointOfInterestViewModel
+            {
+                Description = pointOfInterestFromStore.Description,
+                Name = pointOfInterestFromStore.Name
+            };
+            patchDoc.ApplyTo(pointOfInterestTOPatch, ModelState);
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            if (! TryValidateModel(pointOfInterestTOPatch))
+            {
+                return BadRequest(ModelState);
+            }
+            pointOfInterestFromStore.Name = pointOfInterestTOPatch.Name;
+            pointOfInterestFromStore.Description = pointOfInterestTOPatch.Description;
+            return NoContent();
+            /*
+             Shape of request
+                [
+                  {
+                    "op": "replace"
+                    "path": "/name",
+                    "value": "update from patch",
+                  }
+                ]
+             */
         }
 
         // DELETE api/values/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public IActionResult DeletePointOfInterest(int cityId,int id)
         {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+            if (city == null)
+            {
+                return NotFound();
+            }
+            var pointOfInterestToBeDeleted = city.PointsOfInterest.FirstOrDefault(p => p.Id == id);
+            if (pointOfInterestToBeDeleted == null)
+            {
+                return NotFound();
+            }
+            city.PointsOfInterest.Remove(pointOfInterestToBeDeleted);
+            return NoContent();
         }
     }
 }
---
+++ CityInfo.API/ViewModels/PointOfInterestViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API.ViewModels
+{
+    public class PointOfInterestViewModel
+    {
+        [Required]
+        [MaxLength(50,ErrorMessage ="Name is very long")]
+        public string Name { get; set; }
+
+        [MaxLength(200,ErrorMessage ="Descreption is very long")]
+        [Required]
+        public string Description { get; set; }
+
+    }
+}
